7086e35ab6971e6b751428cef4edc3b2e239fe7e
Jaaaniss <janis.liedeskalnins@valmierastehnikums.lv>
2023-02-14 17:53:38 +0400

    bik

--- resources/views/sakums.blade.php
+++ resources/views/sakums.blade.php
@@ -32,9 +32,6 @@
             aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-            <div class="registers">
-                <a id="a1" href="/register">Create a new account!</a>
-            </div>
             <div class="deda">
                 <img onclick="darkmode()" id="mybtn" src="{{ asset('/image/moon.png') }}" height="30px" width="30px">
             </div>
@@ -45,12 +42,6 @@
                           <li>
                                <div class="row">
                                       <div class="col-md-12">
-                                          Login via
-                                          <div class="social-buttons">
-                                              <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
-                                              <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
-                                          </div>
-                                          or
                                            <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
                                                   <div class="form-group">
                                                        <label class="sr-only" for="exampleInputEmail2">Email address</label>
@@ -64,15 +55,10 @@
                                                   <div class="form-group">
                                                        <button type="submit" class="btn btn-primary btn-block">Sign in</button>
                                                   </div>
-                                                  <div class="checkbox">
-                                                       <label>
-                                                       <input type="checkbox"> keep me logged-in
-                                                       </label>
-                                                  </div>
                                            </form>
                                       </div>
                                       <div class="bottom text-center">
-                                          New here ? <a href="#"><b>Join Us</b></a>
+                                          New here ? <a href="/register"><b>Join Us</b></a>
                                       </div>
                                </div>
                           </li>
